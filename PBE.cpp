cd3b52a23a5f9ff658ffc52c204f3dbf387f2b65
Nathan Perkins <nathanlanep@gmail.com>
2016-11-09 17:00:37 -0600

    Update PBE.cpp

--- PBE.cpp
+++ PBE.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int main() 
 {
 	ShowWindow( GetConsoleWindow(), SW_HIDE );
-    system("explorer C:\\Users\\Greg\\Desktop\\You know... the stuff\\League\\PBE\\lol.launcher.exe ");
+	system("explorer C:\\Users\\Greg\\Desktop\\You know... the stuff\\League\\PBE\\lol.launcher.exe ");
 }
